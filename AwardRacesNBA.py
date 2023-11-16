4beaa92a7addac79bcfc9292041c54a4559dde27
Avinaash Singh Saini <avinaash.saini97@hotmail.com>
2023-11-15 19:28:20 -0800

    working NBA and Soccer top10 lists

--- AwardRacesNBA.py
+++ AwardRacesNBA.py
@@ -1,6 +1,7 @@
 import http.client
 import json
 
+#Below was done with the help of the SportsRadar API documentation
 url = "/nba/trial/v8/en/seasons/2023/REG/leaders.json?api_key=2595gy5scfm5sgje8fa4de36"
 
 conn = http.client.HTTPSConnection("api.sportradar.us")
@@ -14,73 +15,108 @@ conn.close()
 
 LeadersJSON = json.loads(decodedData)
 
-with open ("leaders.json","w") as file:
+#store JSON data
+with open ("NBALeaders.json","w") as file:
     json.dump(LeadersJSON,file,indent=4)
 
-with open("leaders.json","r") as reading:
+#read JSON data
+with open("NBALeaders.json","r") as reading:
     DataSc = json.load(reading)
    
+   #find and store a set of data correnspoinding to the apporiate statistical race
 for category in DataSc['categories']:
+    # Scoring champ
     if category['name'] == 'points' and category['type'] == 'average':
         pointsPerGame = category
+    # Assist champ
     elif category['name'] == 'assists' and category['type'] == 'average':
         assistPerGame = category
+    # Rebounding Champ
     elif category['name'] == 'rebounds' and category['type'] == 'average':
         reboundsPerGame = category
+    # Block Champ
     elif category['name'] == 'blocks' and category['type'] == 'average':
         blocksPerGame = category
+    # Steals Champ
     elif category['name'] == 'steals' and category['type'] == 'average':
         stealsPerGame = category
 
+#functions to print out to terminal the top 10 in each of the 5 major categories
 def scoringChampRace(pointsPerGame):
     if pointsPerGame is not None:
         ranks = pointsPerGame['ranks'][:10]
+        i=0
         print("Top 10:")
         for rank in ranks:
-            print(rank['player']['full_name'],"--->",rank['score'])
+            if i==10:
+                break
+            else:
+                i+=1
+                print(f"{i}.{rank['player']['full_name']}   {rank['score']} PPG")
     else:
         print("Category not found.")
 
 def assistChampRace(assistPerGame):
     if assistPerGame is not None:
         ranks = assistPerGame['ranks'][:10]
+        i=0
         print("Top 10:")
         for rank in ranks:
-            print(rank['player']['full_name'],"--->",rank['score'])
+            if i==10:
+                break
+            else:
+                i+=1
+                print(f"{i}.{rank['player']['full_name']}   {rank['score']} APG")
     else:
         print("Category not found.")
 
 def ReboundChampRace(reboundsPerGame):
     if reboundsPerGame is not None:
         ranks = reboundsPerGame['ranks'][:10]
+        i=0
         print("Top 10:")
         for rank in ranks:
-            print(rank['player']['full_name'],"--->",rank['score'])
+            if i==10:
+                break
+            else:
+                i+=1
+                print(f"{i}.{rank['player']['full_name']}   {rank['score']} RPG")
     else:
         print("Category not found.")
 
 def BlockChampRace(blocksPerGame):
     if blocksPerGame is not None:
         ranks = blocksPerGame['ranks'][:10]
+        i=0
         print("Top 10:")
         for rank in ranks:
-            print(rank['player']['full_name'],"--->",rank['score'])
+            if i==10:
+                    break
+            else:
+                i+=1
+                print(f"{i}.{rank['player']['full_name']}   {rank['score']} BPG")
     else:
         print("Category not found.")
 
 def StealsChampRace(stealsPerGame):
     if stealsPerGame is not None:
         ranks = stealsPerGame['ranks'][:10]
+        i=0
         print("Top 10:")
         for rank in ranks:
-            print(rank['player']['full_name'],"--->",rank['score'])
+            if i==10:
+                    break
+            else:
+                i+=1
+                print(f"{i}.{rank['player']['full_name']}   {rank['score']} SPG")
     else:
         print("Category not found.")
 
+#Some testing to see if stats are accurate compared to NBA.com/stats
 on = True
 while on:
     print("=========================================")
-    choice=input("Please chose one of the following:\n 1.Scoring champ race\n 2. Assist champ race \n 3. Rebound champ race \n 4.Block champ race \n 5.Steal champ race\n")
+    choice=input("Please chose one of the following:\n1.Scoring champ race\n2.Assist champ race \n3.Rebound champ race \n4.Block champ race \n5.Steal champ race\n")
     if choice=="1":
         scoringChampRace(pointsPerGame)
     elif choice=="2":
@@ -93,6 +129,6 @@ while on:
         StealsChampRace(stealsPerGame)
     else:
         on=False
-        print("invalid input\n Goodbye!")
+        print("Exiting...Goodbye!")
     
         
