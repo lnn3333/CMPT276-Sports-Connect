96874305528e539ccbd88c2e32e21fb7d1290fe3
Avinaash Singh Saini <avinaash.saini97@hotmail.com>
2023-12-03 15:50:01 -0800

    Created 5 unit test files under test directory

---
+++ tests/test-news.py
@@ -0,0 +1,16 @@
+import unittest
+import json
+from news_api import getNews
+
+class TestNewsFunctions(unittest.TestCase):
+
+    def setUp(self):
+        with open("tests/sample-newsData.json","r") as reading:
+            self.newsData = json.load(reading)
+        
+    def testGetNews(self):    
+        newsArticles = getNews(self.newsData)
+        self.assertIsNotNone(newsArticles)
+
+if __name__ == '__main__':
+    unittest.main()
