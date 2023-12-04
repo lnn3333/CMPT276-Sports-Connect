5dd98ed16bce16008110b0e4dc45f658d1134ecf
Avinaash Singh Saini <avinaash.saini97@hotmail.com>
2023-12-03 18:40:01 -0800

    added file for integration test

---
+++ test-integration.py
@@ -0,0 +1,58 @@
+import unittest
+from app import app
+
+# integration tests to ensure the web app returns a 200 code (request was successful)
+class TestIntegrationApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+    
+    def testHomePage(self):
+        webResponse = self.app.get("/")
+        code = webResponse.status_code
+        self.assertEqual(code,200)
+        
+    def testAwardRaces(self):
+        webResponse = self.app.get("/nbaAwards")
+        code = webResponse.status_code
+        self.assertEqual(code,200)
+        
+    def testSchedule(self):
+        webResponse = self.app.get("/schedule")
+        code = webResponse.status_code
+        self.assertEqual(code,200)
+
+    def testteamList(self):
+        webResponse = self.app.get("/team")
+        code = webResponse.status_code
+        self.assertEqual(code,200)        
+
+    def testteamSchedule(self):
+        webResponse = self.app.get("/team-schedule")
+        code = webResponse.status_code
+        self.assertEqual(code,200)
+        
+    def testPlayerProfile(self):
+        webResponse = self.app.get("/player-profile")
+        code = webResponse.status_code
+        self.assertEqual(code,200)        
+
+    def testteamProfile(self):
+        webResponse = self.app.get("/team-profile")
+        code = webResponse.status_code
+        self.assertEqual(code,200)
+        
+    def testpastSeason(self):
+        webResponse = self.app.get("/pastSeason")
+        code = webResponse.status_code
+        self.assertEqual(code,200)        
+        
+    def testplayerStats(self):
+        webResponse = self.app.get("/player-statistics")
+        code = webResponse.status_code
+        self.assertEqual(code,200)               
+
+    def testteamStats(self):
+        webResponse = self.app.get("/team-statistics")
+        code = webResponse.status_code
+        self.assertEqual(code,200)
+        
